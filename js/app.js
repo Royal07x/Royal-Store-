53765bc5beaebea7bd63fe5340fea8b97fdb3b40
Royal fashion <himanshukumar12289@gmail.com>
2026-08-11 09:13:03 +0530

    Add total price calculation for cart

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 let cart = 0;
+let total = 0;
 
 const cartCount = document.getElementById("cart-count");
+const totalPrice = document.getElementById("total-price");
+
 const buttons = document.querySelectorAll(".product button");
 
-buttons.forEach(button => {
+const prices = [799, 999, 499];
+
+buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
     cart++;
+    total += prices[index];
+
     cartCount.textContent = cart;
+    totalPrice.textContent = total;
+
     alert("Product Cart me add ho gaya!");
   });
 });
